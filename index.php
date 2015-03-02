b168918a15bb9324a4223c800927948779652ac6
Kirby <kherbz07@gmail.com>
2015-03-02 15:03:35 +0800

    Edit index and added jquery and script_odd.

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 <title>Magic Square</title>
 </head>
 <body>
-
+<form>
+<input type="text" id="square" name="square" />
+<button id="create">Display Magic Square</button>
+</form>
+<div id="content"></div>
 </body>
 </html>
